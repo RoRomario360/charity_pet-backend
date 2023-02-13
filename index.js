f15b8c665adf3f8876660a036c31367940287ff0
RMerkulov <fotoromario@gmail.com>
2023-02-13 10:58:00 +0200

    upd

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 import express from "express";
-import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
 import mongoose from "mongoose";
-import { validationResult } from "express-validator";
-import UserModel from "./models/User.js";
-
+import checkAuth from "./utils/checkAuth.js";
 import { registerValidation } from "./validations/auth.js";
+import { getMe, login, register } from "./controllers/UserController.js";
 
 mongoose
   .connect(
@@ -17,34 +14,14 @@ mongoose
 const app = express();
 app.use(express.json());
 
+//authorize user
+app.post("/auth/login", login);
+
 //register of users
-app.post("/auth/register", registerValidation, async (req, res) => {
-  try {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json(errors.array());
-    }
+app.post("/auth/register", registerValidation, register);
 
-    const password = req.body.password;
-    const salt = await bcrypt.genSalt(10);
-    const passwordHash = await bcrypt.hash(password, salt);
-
-    const doc = new UserModel({
-      email: req.body.email,
-      fullName: req.body.fullName,
-      avatarUrl: req.body.avatarUrl,
-      passwordHash,
-    });
-
-    const user = await doc.save();
-
-    res.json(user);
-  } catch (err) {
-    res.status(500).json({
-      message: "Can't registered",
-    });
-  }
-});
+//info for user
+app.get("/auth/me", checkAuth, getMe);
 
 app.listen(4000, (err) => {
   if (err) {
